06134c246a5185123494f0403dab9a72dd8c9ab3
DESKTOP-G3EI2ED\Павел <pavel.chernenko97@gmail.com>
2023-02-01 13:41:27 +0300

    feat: configure react-router and add homepage

---
+++ src/router/router.tsx
@@ -0,0 +1,15 @@
+import { HomePage } from 'pages';
+import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+
+import { ROUTE } from './routes';
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path={ROUTE.HOME}>
+      <Route element={<HomePage />} />
+      <Route path={ROUTE.PROFILE} />
+      <Route path={ROUTE.SIGN_IN} />
+      <Route path={ROUTE.SIGN_IN} />
+    </Route>,
+  ),
+);
