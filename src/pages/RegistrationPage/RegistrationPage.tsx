6e0f5b361529f7bd925660f824857a37d0b8c834
Demyankov PC job <demyantaurus@mail.ru>
2023-02-14 10:42:01 +0300

    fix:  changed registration page styles

--- src/pages/RegistrationPage/RegistrationPage.tsx
+++ src/pages/RegistrationPage/RegistrationPage.tsx
@@ -1,21 +1,32 @@
 import { RegistrationForm } from 'modules';
 
+import { Link } from 'react-router-dom';
+
+import { ROUTE } from 'router';
+
 import { icons } from './data/icons';
 
 import styles from './styles.module.scss';
 
+import { logoIcon } from '../../assets';
+
 export const RegistrationPage = () => (
   <main className={styles.wrapper}>
-    <div className={styles.about}>
-      <h1>Помогаем автоматизировать бизнес-процессы с помощью чат-ботов</h1>
-      <ul className={styles.socialList}>
-        {icons.map((Icon, i) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <li key={i}>
-            <Icon />
-          </li>
-        ))}
-      </ul>
+    <div className={styles.aboutWrapper}>
+      <div className={styles.about}>
+        <Link to={ROUTE.HOME} className={styles.logo}>
+          <img src={logoIcon} className={styles.logo} alt="Townsend logo" />
+        </Link>
+        <h1>Помогаем автоматизировать бизнес-процессы с помощью чат-ботов</h1>
+        <ul className={styles.socialList}>
+          {icons.map((Icon, i) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <li key={i}>
+              <Icon />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
     <div className={styles.registration}>
       <RegistrationForm />
