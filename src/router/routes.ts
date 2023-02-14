b39f24596092f34cd37d3607815207fb943269cb
Demyankov PC job <demyantaurus@mail.ru>
2023-02-14 09:24:18 +0300

    fix:  changed enum ROUTE

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,6 +1,6 @@
 export enum ROUTE {
   HOME = '/',
-  PROFILE = 'profile',
-  SIGN_UP = 'sign-up',
-  SIGN_IN = 'sign-in',
+  PROFILE = '/profile',
+  SIGN_UP = '/sign-up',
+  SIGN_IN = '/sign-in',
 }
