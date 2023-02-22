12127446f571a5037b4823560b1e136c5522cbc4
AgeevDima <dimasageev1@gmail.com>
2023-02-22 09:36:39 +0300

    fix: lint fix

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,12 @@
 import styles from './styles.module.scss';
 
-export const HomePage = () => <div className={styles.test}>HomePage</div>;
+import buttonStyles from '../../styles/shared/button.style.module.scss';
+
+export const HomePage = () => (
+  <div className={styles.test}>
+    HomePage
+    <button className={buttonStyles.buttonDark} type="button">
+      ghghghg
+    </button>
+  </div>
+);
