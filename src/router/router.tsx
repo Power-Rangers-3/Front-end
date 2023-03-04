d1e39d41078ed8ff2aeff56f33727d728190ed34
Pavel <pavel.chernenko97@gmail.com>
2023-03-04 12:38:31 +0300

    fix: barrel pattern for platform page

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,19 +1,18 @@
 import {
-    HomePage,
-    MainLayout,
-    NewPasswordPage,
-    ProfileLayout,
-    RegistrationPage,
-    ResetPasswordPage,
-    ProfileSettingsPage,
-    SignInPage,
-    AdminPageLayout,
-    ProfileDashboardPage,
-    ProfileFavoritePage,
-    ProfileHistoryPage,
+  HomePage,
+  MainLayout,
+  NewPasswordPage,
+  ProfileLayout,
+  RegistrationPage,
+  ResetPasswordPage,
+  ProfileSettingsPage,
+  SignInPage,
+  AdminPageLayout,
+  ProfileDashboardPage,
+  ProfileFavoritePage,
+  ProfileHistoryPage,
+  PlatformsPage,
 } from 'pages';
-
-import { PlatformsPage } from 'pages/PlatformsPage/PlatformsPage';
 
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 import { IUser } from 'store/types';
@@ -23,35 +22,34 @@ import { ProtectedAdminPage } from './ProtectedAdminPage';
 import { ROUTE } from './routes';
 
 export const router = (user: IUser) =>
-    createBrowserRouter(
-        createRoutesFromElements(
-            <>
-                <Route path={ROUTE.HOME} element={<MainLayout />}>
-                    <Route index element={<HomePage />} />
-                    <Route path={ROUTE.PLATFORMS} element={<PlatformsPage />} />
-                </Route>
-                <Route path={ROUTE.PROFILE} element={<ProfileLayout />}>
-                    <Route path={ROUTE.PROFILE} element={<ProfileDashboardPage />}>
-                        <Route index element={<ProfileFavoritePage />} />
-                        <Route path={ROUTE.PROFILE_FAVORITE} element={<ProfileFavoritePage />} />
-                        <Route path={ROUTE.PROFILE_HISTORY} element={<ProfileHistoryPage />} />
-                    </Route>
-                    <Route path={ROUTE.PROFILE_SETTINGS} element={<ProfileSettingsPage />} />
-                </Route>
-                <Route path={ROUTE.SIGN_UP} element={<RegistrationPage />} />
-                <Route path={ROUTE.SIGN_IN} element={<SignInPage />} />
-                <Route path={ROUTE.RESET_PASSWORD} element={<ResetPasswordPage />}>
-                    <Route path={`${ROUTE.RESET_PASSWORD}/:token`} element={<SignInPage />} />
-                </Route>
-                <Route path={ROUTE.NEW_PASSWORD} element={<NewPasswordPage />} />
-                <Route
-                    path={ROUTE.ADMIN}
-                    element={
-                        <ProtectedAdminPage user={user}>
-                            <AdminPageLayout />
-                        </ProtectedAdminPage>
-                    }
-                />
-            </>,
-        ),
-    );
+  createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path={ROUTE.HOME} element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path={ROUTE.PLATFORMS} element={<PlatformsPage />} />
+        </Route>
+        <Route path={ROUTE.PROFILE} element={<ProfileLayout />}>
+          <Route path={ROUTE.PROFILE} element={<ProfileDashboardPage />}>
+            <Route index path={ROUTE.PROFILE_FAVORITE} element={<ProfileFavoritePage />} />
+            <Route path={ROUTE.PROFILE_HISTORY} element={<ProfileHistoryPage />} />
+          </Route>
+          <Route path={ROUTE.PROFILE_SETTINGS} element={<ProfileSettingsPage />} />
+        </Route>
+        <Route path={ROUTE.SIGN_UP} element={<RegistrationPage />} />
+        <Route path={ROUTE.SIGN_IN} element={<SignInPage />} />
+        <Route path={ROUTE.RESET_PASSWORD} element={<ResetPasswordPage />}>
+          <Route path={`${ROUTE.RESET_PASSWORD}/:token`} element={<SignInPage />} />
+        </Route>
+        <Route path={ROUTE.NEW_PASSWORD} element={<NewPasswordPage />} />
+        <Route
+          path={ROUTE.ADMIN}
+          element={
+            <ProtectedAdminPage user={user}>
+              <AdminPageLayout />
+            </ProtectedAdminPage>
+          }
+        />
+      </>,
+    ),
+  );
