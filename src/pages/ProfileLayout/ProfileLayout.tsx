7393272398d27a453f7f01398054a07218f30359
Pavel <pavel.chernenko97@gmail.com>
2023-03-04 14:03:05 +0300

    refactor(profile): add array for nav links and rename styles

--- src/pages/ProfileLayout/ProfileLayout.tsx
+++ src/pages/ProfileLayout/ProfileLayout.tsx
@@ -7,6 +7,45 @@ import { ProfileIcon } from './ProfileIcon/ProfileIcon';
 
 import styles from './styles.module.scss';
 
+const routes = [
+  {
+    to: ROUTE.PROFILE_FAVORITE,
+    img: myPageIcon,
+    className: styles.link,
+    text: 'Дашборд',
+  },
+  {
+    to: ROUTE.PROFILE,
+    img: materialsIcon,
+    className: styles.link,
+    text: 'Подобрать решение',
+  },
+  {
+    to: ROUTE.PROFILE_PLATFORMS,
+    img: myHistoryIcon,
+    className: styles.link,
+    text: 'Подобрать платформу',
+  },
+  {
+    to: ROUTE.PROFILE,
+    img: walletIcon,
+    className: styles.link,
+    text: 'Профиль',
+  },
+  {
+    to: ROUTE.PROFILE,
+    img: ratesIcon,
+    className: styles.link,
+    text: 'Тарифы',
+  },
+  {
+    to: ROUTE.PROFILE_SETTINGS,
+    img: settingsIcon,
+    className: styles.link,
+    text: 'Настройки',
+  },
+];
+
 export const ProfileLayout = () => {
   const { name, fullname } = useAppSelector(getUser);
 
@@ -19,30 +58,12 @@ export const ProfileLayout = () => {
         <Link to={ROUTE.HOME}>
           <img src={logoIcon} className={styles.logo} alt="Townsend logo" />
         </Link>
-        <Link to={ROUTE.PROFILE} className={styles.nav__link}>
-          <img src={myPageIcon} className={styles.nav__logo} alt="Дашборд" />
-          Дашборд
-        </Link>
-        <Link to={ROUTE.PROFILE} className={styles.nav__link}>
-          <img src={materialsIcon} className={styles.nav__logo} alt="Подобрать решение" />
-          Подобрать решение
-        </Link>
-        <Link to={ROUTE.PROFILE} className={styles.nav__link}>
-          <img src={myHistoryIcon} className={styles.nav__logo} alt="Подобрать платформу" />
-          Подобрать платформу
-        </Link>
-        <Link to={ROUTE.PROFILE} className={styles.nav__link}>
-          <img src={walletIcon} className={styles.nav__logo} alt="Профиль" />
-          Профиль
-        </Link>
-        <Link to={ROUTE.PROFILE} className={styles.nav__link}>
-          <img src={ratesIcon} className={styles.nav__logo} alt="Тарифы" />
-          Избранное
-        </Link>
-        <Link to={ROUTE.PROFILE_SETTINGS} className={styles.nav__link}>
-          <img src={settingsIcon} className={styles.nav__logo} alt="Настройки" />
-          Настройки
-        </Link>
+        {routes.map(({ to, img, className, text }) => (
+          <Link to={to} className={className}>
+            <img src={img} className={styles.icon} alt={text} />
+            {text}
+          </Link>
+        ))}
       </nav>
       <div className={styles.outlet}>
         <Outlet />
