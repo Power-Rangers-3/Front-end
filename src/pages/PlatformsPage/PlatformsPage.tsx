632e31931c58010371180a8ece14f5a285d9bd68
Pavel <pavel.chernenko97@gmail.com>
2023-03-06 02:16:29 +0300

    feat(pages): add settings for pagination

--- src/pages/PlatformsPage/PlatformsPage.tsx
+++ src/pages/PlatformsPage/PlatformsPage.tsx
@@ -12,13 +12,20 @@ export const PlatformsPage = () => {
     dispatch(addFavoritePlatform(id));
   };
   const [currentPage, setCurrentPage] = useState(1);
+  const ELEMENT_FOR_PAGE = 10;
+  const lastIndexElement = currentPage * ELEMENT_FOR_PAGE;
+  const firstIndexElement = lastIndexElement - ELEMENT_FOR_PAGE;
   const handlePageChange = (pageNumber: number) => {
     setCurrentPage(pageNumber);
   };
   return (
     <section className={styles.section}>
       <div className={styles.wrapper}>
-        <PlatformList platforms={platforms} isAuth={isAuth} onFavorite={addToFavorite} />
+        <PlatformList
+          platforms={platforms.slice(firstIndexElement, lastIndexElement)}
+          isAuth={isAuth}
+          onFavorite={addToFavorite}
+        />
         <Pagination
           currentPage={currentPage}
           totalPages={Math.ceil(platforms.length / 10)}
