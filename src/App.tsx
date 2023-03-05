15918c67e120091e93ac411c190ee50641facd36
Pavel <pavel.chernenko97@gmail.com>
2023-03-06 00:19:51 +0300

    fix: bug with set user

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { RouterProvider } from 'react-router';
 import { router } from 'router';
-import { getUser, useAppSelector } from 'store';
+import { getUser, setPlatforms, signInAction, useAppDispatch, useAppSelector } from 'store';
 import { Global } from '@emotion/react';
 import { getGlobalStyles } from 'styles/globalStyles';
+import { useEffect } from 'react';
+import { platformsData } from 'mocks';
 
 export const App = () => {
   const user = useAppSelector(getUser);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(setPlatforms(platformsData));
+    dispatch(signInAction());
+  }, [dispatch]);
 
   return (
     <>
