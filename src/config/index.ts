de268498d895d5842ffdc07c2018fe68772d2daa
Valentina <matsulevichvalentina@gmail.com>
2023-03-08 18:31:41 +0300

    taking domain from location origin instead of host

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,4 +2,4 @@ export * from './portal';
 
 export const isProduction = process.env.NODE_ENV !== 'development';
 export const pathPrefix = isProduction ? `/api` : '';
-export const domain = process.env.REACT_APP_API_AUTH ?? window?.location?.host ?? '127.0.0.1';
+export const domain = process.env.REACT_APP_API_AUTH ?? window?.location?.origin ?? '127.0.0.1';
