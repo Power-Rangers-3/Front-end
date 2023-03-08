27ba8a0edf8d910f4f381e8af1c0134c2b269904
Valentina <matsulevichvalentina@gmail.com>
2023-03-08 16:30:47 +0300

    domain fix

--- src/modules/Auth/api/userSignIn.ts
+++ src/modules/Auth/api/userSignIn.ts
@@ -1,11 +1,11 @@
 import { LocalStorage } from 'shared/localStorage/localStorage';
-import { baseUrl } from 'config';
+import { pathPrefix } from 'config';
 
 import { authAxiosInstance } from './authAxiosInstance';
 
 import { IUserResponse } from '../types';
 
-const apiPath = `${baseUrl}/users/info/`;
+const apiPath = `${pathPrefix}/users/info/`;
 
 export const userSignIn = async (): Promise<IUserResponse> => {
   const accessToken = localStorage.getItem(LocalStorage.AccessToken);
